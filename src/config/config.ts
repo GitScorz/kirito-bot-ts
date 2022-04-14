0a5e81d5e2b871ff953dcb3d3f7dc1e9ff286691
Scorz <cristalizado6@gmail.com>
2022-04-14 17:28:40 +0100

    tweak(config): idle rgb color

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 import { ColorResolvable } from "discord.js";
 
 export const BOT_PREFIX: string = "k!";
+
 export const BOT_GLOBAL_RGB_COLOR: ColorResolvable = [59, 112, 223];
+export const BOT_IDLE_RGB_COLOR: ColorResolvable = [255, 150, 31];

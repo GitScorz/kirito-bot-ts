1434ed74b178858d663363d1e6783571abb55508
Scorz <cristalizado6@gmail.com>
2022-04-14 23:12:18 +0100

    tweak(utils): add error embed function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { TextChannel, User, MessageEmbed } from "discord.js";
+
 export function parseMS(ms: number) { // Parse milliseconds to a human readable string
   const seconds = Math.floor(ms / 1000);
   const minutes = Math.floor(seconds / 60);
@@ -8,4 +10,12 @@ export function parseMS(ms: number) { // Parse milliseconds to a human readable 
   const h = hours % 24;
 
   return `${h}h:${m}m:${s}s`;
+}
+
+export function ErrorEmbed(channel: any, user: User, message: string) {
+  const err = new MessageEmbed()
+    .setDescription("**" + user.username + "**, " + message)
+    .setColor([255, 0, 0]);
+
+  return channel.send({ embeds: [err] });
 }

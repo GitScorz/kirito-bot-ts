5dde36bb063fc342b17dea4c3801913a6436fd19
Scorz <cristalizado6@gmail.com>
2022-04-14 17:29:11 +0100

    tweak(commands): ping command embed

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,5 +1,6 @@
 import { Command } from "@sapphire/framework";
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
+import { BOT_GLOBAL_RGB_COLOR, BOT_IDLE_RGB_COLOR } from "../../config/config";
 
 export class PingCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
@@ -12,6 +13,31 @@ export class PingCommand extends Command {
   }
 
   public async messageRun(message: Message): Promise<Message> {
-    return message.channel.send(`Pong! Latency is\` ${this.container.client.ws.ping}ms\``);
+    const currentDate = Date.now();
+    const heartbeat = (currentDate - message.createdTimestamp);
+
+    const calculatingEmbed = new MessageEmbed()
+      .setDescription("Calculating..")
+      .setColor(BOT_IDLE_RGB_COLOR)
+
+    const m = await message.channel.send({ embeds: [calculatingEmbed] });
+
+    const latency = Math.round(m.createdTimestamp - message.createdTimestamp);
+    const apiLatency = Math.round(this.container.client.ws.ping);
+
+    let msg = `\nLatency » \`${latency}ms\``;
+    // msg += `\nHeartbeat » \`${heartbeat}ms\``;
+    msg += `\nAPI Latency » \`${apiLatency}ms\``;
+
+    const pongEmbed = new MessageEmbed()
+      .setAuthor({
+        name: "Latency!",
+        iconURL: this.container.client.user.displayAvatarURL({ format: "png", dynamic: true })
+      })
+      .setDescription(msg)
+      .setColor(BOT_GLOBAL_RGB_COLOR)
+      .setTimestamp();
+
+    return m.edit({ embeds: [pongEmbed] });
   }
 }

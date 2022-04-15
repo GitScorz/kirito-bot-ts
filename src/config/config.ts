6c74ce08e5a45b096c857c313b84c10ac9d87eb2
Scorz <cristalizado6@gmail.com>
2022-04-15 12:22:57 +0100

    feat(config): invisible rgb color

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ export const BOT_PREFIX: string = "k!";
 
 export const BOT_GLOBAL_RGB_COLOR: ColorResolvable = [59, 112, 223];
 export const BOT_IDLE_RGB_COLOR: ColorResolvable = [255, 150, 31];
+export const BOT_INVISIBLE_RGB_COLOR: ColorResolvable = [54, 57, 64];
 
 export const BOT_PRESENCE: PresenceData = {
   status: 'online',

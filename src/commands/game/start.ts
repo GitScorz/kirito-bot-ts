7a26511248145feee265c7d47e20f5ec42f8bc43
Scorz <cristalizado6@gmail.com>
2022-04-15 11:45:19 +0100

    tweak(commands/start): character going into db

--- src/commands/game/start.ts
+++ src/commands/game/start.ts
@@ -1,7 +1,8 @@
 import { Args, Command } from "@sapphire/framework";
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 import { ErrorEmbed } from "../../utils/utils";
 import Character from '../../schemas/Character';
+import { BOT_GLOBAL_RGB_COLOR } from "../../config/config";
 
 export class StartCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
@@ -13,16 +14,21 @@ export class StartCommand extends Command {
   }
 
   public async messageRun(message: Message, args: Args): Promise<Message> {
-    let user = message.author;
+    const user = message.author;
+    let emptyName = false;  // Check if the name is empty
+
     const char = await Character.findOne({ userId: user.id });
 
     if (char) {
       return ErrorEmbed(message.channel, user, "You already have a character!");
     }
 
-    let name = await args.rest('string');
-    if (!name) {
-      return ErrorEmbed(message.channel, user, "You must enter a name!");
+    let name: any = await args.rest('string').catch(() => {
+      emptyName = true;
+    });
+
+    if (emptyName) {
+      return ErrorEmbed(message.channel, user, "You need to specify a name!");
     }
 
     const charName = await Character.findOne({ name: name });
@@ -33,8 +39,39 @@ export class StartCommand extends Command {
     if (name.length > 20) {
       return ErrorEmbed(message.channel, user, "Your name must be 20 characters or less!");
     }
-    
 
-    return message.channel.send(`${name} has started their adventure!`);
+
+    try {  // Create the character
+      Character.create({
+        userId: user.id,
+        name: name,
+        gold: 2000,
+        troops: 200,
+        health: 100,
+        gems: 500,
+        alliance: "None",
+        potionEffect: "None",
+        wins: 0,
+        loss: 0,
+        matches: 0
+      });
+
+      let msg = `Your character is named **${name}**.`;
+      msg += `\nYou got **${2000}** gold.`;
+      msg += `\nYou got **${200}** troops.`;
+      msg += `\nYou got **${500}** gems.`;
+      msg += `\nYou can join in an alliance with the command \`k!alliance\`.`;
+
+      let embed = new MessageEmbed()
+        .setAuthor({ name: user.username, iconURL: user.displayAvatarURL() })
+        .setTitle("Your character has been created!")
+        .setDescription(msg)
+        .setColor(BOT_GLOBAL_RGB_COLOR);
+
+      message.channel.send({ embeds: [embed] });
+    } catch (err) {  // Catch any errors
+      console.error(err);
+      ErrorEmbed(message.channel, user, "there was an error while creating your character.")
+    }
   }
 }

8ed52b8159f1cbcc07121c9d9b3422f36c8f2764
Scorz <cristalizado6@gmail.com>
2022-04-15 12:01:13 +0100

    feat(commands): add gold comand

---
+++ src/commands/game/gold.ts
@@ -0,0 +1,29 @@
+import { Command } from "@sapphire/framework";
+import { Message, MessageEmbed } from "discord.js";
+import { BOT_GLOBAL_RGB_COLOR } from "../../config/config";
+import Character from "../../schemas/Character";
+
+export class GoldCommand extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, {
+      ...options,
+      description: "See you current gold!",
+      fullCategory: ["game"],
+    });
+  }
+
+  public async messageRun(message: Message): Promise<Message> {
+    const user = message.author;
+
+    const char = await Character.findOne({ userId: user.id });
+    if (!char) {
+      return message.channel.send("You don't have a character!");
+    }
+
+    let embed = new MessageEmbed()
+      .setDescription(`**${user.username}**, you currently have **${char.gold}** gold!`)
+      .setColor(BOT_GLOBAL_RGB_COLOR)
+    
+    return message.channel.send({ embeds: [embed] });
+  }
+}

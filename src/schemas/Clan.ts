53f0e8937d98e499683c2b7d59ce3505268e55d0
Scorz <cristalizado6@gmail.com>
2022-04-23 16:53:11 +0100

    tweak(schemas/clan): new options

--- src/schemas/Clan.ts
+++ src/schemas/Clan.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const ClanSchema = new mongoose.Schema({
   name: mongoose.SchemaTypes.String,
-  users: mongoose.SchemaTypes.Number,
+  description: mongoose.SchemaTypes.String,
+  members: mongoose.SchemaTypes.Number,
   minimumWins: mongoose.SchemaTypes.Number,
-  state: mongoose.SchemaTypes.String,
+  open: mongoose.SchemaTypes.String,
   owner: mongoose.SchemaTypes.String,
   createdAt: mongoose.SchemaTypes.Number
 });

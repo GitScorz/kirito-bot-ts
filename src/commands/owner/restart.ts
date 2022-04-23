85ab7b1b30860f694e1d84c0921f4713f5ad5ac0
Scorz <cristalizado6@gmail.com>
2022-04-23 18:41:11 +0100

    feat(commands): restart command

---
+++ src/commands/owner/restart.ts
@@ -0,0 +1,23 @@
+import { Command } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export class EvalCommand extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, {
+      ...options,
+      description: "Restarts the bot.",
+      preconditions: ["OwnerOnly"],
+      fullCategory: ["owner"]
+    })
+  }
+
+  public async messageRun(message: Message) {
+    let started = Date.now();
+    this.container.logger.info(`[WARNING] - BOT IS RESTARTING AS REQUESTED BY ${message.author.username.toUpperCase()}`);
+    
+    message.channel.send({ content: ':recycle: ***Restarting...***' }).then(() => this.container.client.destroy())
+    .then(() => this.container.client.login(process.env.TOKEN));
+
+    this.container.logger.info(`[INFO] - BOT RESTARTED IN ${Date.now() - started}ms`);
+  }
+}

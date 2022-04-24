e3baf4810fec916d1f5a4c99f65ab7356cbf89e5
Scorz <cristalizado6@gmail.com>
2022-04-24 17:40:16 +0100

    tweak(commands/clan): add clan leave

--- src/commands/game/clan.ts
+++ src/commands/game/clan.ts
@@ -1,6 +1,6 @@
 import { Args, Command } from "@sapphire/framework";
-import { Message, MessageEmbed } from "discord.js";
-import { CLAN_MEMBER_LIMIT, BOT_GLOBAL_RGB_COLOR, CLAN_GEM_COST } from "../../config/Config";
+import { ButtonInteraction, Interaction, Message, MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
+import { CLAN_MEMBER_LIMIT, BOT_GLOBAL_RGB_COLOR, CLAN_GEM_COST, BOT_SUCCESS_RGB_COLOR, BOT_ERROR_RGB_COLOR } from "../../config/Config";
 import Character from "../../schemas/Character";
 import Clan from "../../schemas/Clan";
 import { BLACKLISTED_WORDS, ErrorEmbed } from "../../utils/Utils";
@@ -94,7 +94,7 @@ export class BotStatsCommand extends Command {
       let msg = `Your clan \`${clanName}\`has been created!`;
       // msg += "\n\nYou can now invite other members to your clan with \`k!clan invite <user_id>\`";
       msg += `\nYou can now tell your friends to join your clan with \`k!clan join ${clanName}\`.`;
-      msg += "\n\nSee your clan stats with \`k!clan stats\` and change the settings with \`k!clan settings\`.";
+      msg += "\n\nSee your clan stats with \`k!clan\` and change the settings with \`k!clan settings\`.";
 
       const clanEmbed = new MessageEmbed()
         .setTitle("Clan Created!")
@@ -106,7 +106,7 @@ export class BotStatsCommand extends Command {
     }
     
     if (option.toLowerCase() === "join") {
-      if (char?.clanId) return ErrorEmbed(message.channel, user, "you are in a clan!");
+      if (char?.clanId) return ErrorEmbed(message.channel, user, "you are in a clan already!");
 
       const clanName: string = await args.rest('string').catch(() => null);
       if (!clanName) return ErrorEmbed(message.channel, user, "you need to specify a clan name!\nSee the clan list with \`k!clan list\`");
@@ -149,9 +149,176 @@ export class BotStatsCommand extends Command {
       
       const joinEmbed = new MessageEmbed()
         .setTitle("Clan Joined!")
-        .setDescription(`You just joined in **${clanName}**, to get an overview of the clan do \`k!clan overview\``)
+        .setDescription(`You just joined in **${clanName}**, to get an overview of the clan do \`k!clan\``)
         .setColor(BOT_GLOBAL_RGB_COLOR)
       return message.channel.send({ embeds: [joinEmbed] });
+    }
+
+    if (option.toLowerCase() === "leave") {
+      if (!char?.clanId) return ErrorEmbed(message.channel, user, "you aren't in any clan!");
+
+      const clan: IClan = await Clan.findOne({ _id: char.clanId });
+      if (!clan) {  // If clan doesn't exist
+        await Character.updateOne({ userId: user.id }, {
+          $set: { 
+            clanId: null 
+          },
+        });
+
+        const leaveEmbed = new MessageEmbed()
+          .setTitle("Clan Left!")
+          .setDescription("You left your clan!")
+          .setColor(BOT_SUCCESS_RGB_COLOR);
+
+        return message.channel.send({ embeds: [leaveEmbed] });
+      }
+
+      const row = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("yes")
+          .setStyle('SECONDARY')
+          .setEmoji('964494033177157652'),
+        new MessageButton()
+          .setCustomId('no')
+          .setStyle('SECONDARY')
+          .setEmoji('852118537735634965'),
+      );
+
+      if (clan.ownerId === user.id) {  // If you are clan owner
+        if (clan.members.length === 1) {
+          const leaveEmbed = new MessageEmbed()
+            .setTitle("Delete clan?")
+            .setDescription("Are you sure you want to leave your clan and delete it?\nThis action cannot be undone!")
+            .setColor(BOT_ERROR_RGB_COLOR);
+
+          let msg = await message.channel.send({ embeds: [leaveEmbed], components: [row] });
+
+          const filter = (btnInt: Interaction) => {
+            return user.id === btnInt.user.id;
+          };
+  
+          const collector = message.channel.createMessageComponentCollector({
+            filter,
+            max: 1,
+            time: 15 * 1000,  // 1 minute
+          });
+  
+          collector.on('collect', async (i: ButtonInteraction) => {
+            i.deferUpdate()
+            if (i.customId === 'yes') {
+              leaveEmbed.setTitle("Clan Deleted!");
+              leaveEmbed.setDescription("You leaved the clan and deleted it!");
+              leaveEmbed.setColor(BOT_SUCCESS_RGB_COLOR);
+              msg.edit({ embeds: [leaveEmbed] });
+
+              await Character.updateOne({ userId: user.id }, {
+                $set: { 
+                  clanId: null 
+                },
+              });
+
+              await Clan.deleteOne({ _id: char.clanId });
+            } else {
+              leaveEmbed.setTitle("Cancelled!");
+              leaveEmbed.setDescription("You cancelled the action!");
+              leaveEmbed.setColor(BOT_SUCCESS_RGB_COLOR);
+              msg.edit({ embeds: [leaveEmbed] });
+            }
+          });
+        } else {
+          const leaveEmbed = new MessageEmbed()
+            .setTitle("Transfer Ownership?")
+            .setDescription("Are you sure you want to leave the clan and transfer ownership?\nThis action cannot be undone!")
+            .setColor(BOT_ERROR_RGB_COLOR);
+
+          let msg = await message.channel.send({ embeds: [leaveEmbed], components: [row] });
+
+          const filter = (btnInt: Interaction) => {
+            return user.id === btnInt.user.id;
+          };
+  
+          const collector = message.channel.createMessageComponentCollector({
+            filter,
+            max: 1,
+            time: 15 * 1000,  // 1 minute
+          });
+  
+          collector.on('collect', async (i: ButtonInteraction) => {
+            i.deferUpdate()
+            if (i.customId === 'yes') {
+              leaveEmbed.setTitle("Clan Left!");
+              leaveEmbed.setDescription("You leaved the clan and the ownership has been transfered!");
+              leaveEmbed.setColor(BOT_SUCCESS_RGB_COLOR);
+              msg.edit({ embeds: [leaveEmbed] });
+
+              for (let i = 0; i < clan.members.length; i++) {
+                await Clan.updateOne({ _id: char.clanId }, {
+                  $set: {
+                    ownerId: clan.members[i]
+                  }
+                })
+              }
+    
+              await Character.updateOne({ userId: user.id }, {
+                $set: {
+                  clanId: null,
+                },
+              });
+    
+              await Clan.updateOne({ _id: char.clanId }, {
+                $pull: { members: user.id },
+              });
+            } else {
+              leaveEmbed.setTitle("Canceled!");
+              leaveEmbed.setDescription("You cancelled the action!");
+              leaveEmbed.setColor(BOT_SUCCESS_RGB_COLOR);
+              msg.edit({ embeds: [leaveEmbed] });
+            }
+          });
+        }
+      } else { // Members
+        const leaveEmbed = new MessageEmbed()
+          .setTitle("Leave Clan?")
+          .setDescription("Are you sure you want to leave the clan?")
+          .setColor(BOT_ERROR_RGB_COLOR);
+        
+        let msg = await message.channel.send({ embeds: [leaveEmbed], components: [row] });
+
+        const filter = (btnInt: Interaction) => {
+          return user.id === btnInt.user.id;
+        };
+
+        const collector = message.channel.createMessageComponentCollector({
+          filter,
+          max: 1,
+          time: 15 * 1000,  // 1 minute
+        });
+
+        collector.on('collect', async (i: ButtonInteraction) => {
+          i.deferUpdate()
+          if (i.customId === 'yes') {
+            leaveEmbed.setTitle("Clan Left!");
+            leaveEmbed.setDescription("You leaved the clan successfully!");
+            leaveEmbed.setColor(BOT_SUCCESS_RGB_COLOR);
+            msg.edit({ embeds: [leaveEmbed] });
+
+            await Character.updateOne({ userId: user.id }, {
+              $set: { 
+                clanId: null 
+              },
+            });
+
+            await Clan.updateOne({ _id: char.clanId }, {
+              $pull: { members: user.id },
+            });
+          } else {
+            leaveEmbed.setTitle("Cancelled!");
+            leaveEmbed.setDescription("You cancelled the action!");
+            leaveEmbed.setColor(BOT_SUCCESS_RGB_COLOR);
+            msg.edit({ embeds: [leaveEmbed] });
+          }
+        });
+      }
     }
   }
 }
--- src/config/Config.ts
+++ src/config/Config.ts
@@ -8,6 +8,7 @@ export const BOT_VERSION: string = "v2.0.0";
 export const BOT_GLOBAL_RGB_COLOR: ColorResolvable = [59, 112, 223];
 export const BOT_IDLE_RGB_COLOR: ColorResolvable = [255, 150, 31];
 export const BOT_ERROR_RGB_COLOR: ColorResolvable = [255, 0, 0];
+export const BOT_SUCCESS_RGB_COLOR: ColorResolvable = [0, 255, 0];
 export const BOT_INVISIBLE_RGB_COLOR: ColorResolvable = [54, 57, 64];
 
 // export const BOT_INVITE_URL: string = "some url";

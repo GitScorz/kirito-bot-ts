d508d4b218280490831215683facd3f588017f6f
Scorz <cristalizado6@gmail.com>
2022-04-25 13:43:16 +0100

    tweak(commands/daily): update char method

--- src/commands/game/daily.ts
+++ src/commands/game/daily.ts
@@ -21,7 +21,6 @@ export class DailyCommand extends Command {
 
     const cooldown = 8.64e+7;
     const health = char.health;
-    const gold = char.gold;
     const now = Date.now();
 
     const lastClaimed = char.daily?.lastClaimed;
@@ -38,34 +37,25 @@ export class DailyCommand extends Command {
           msg += "\n» **+25**<:life_hp:853288570113359872>";
       }
 
-      const embed = new MessageEmbed()
+      const dailyEmbed = new MessageEmbed()
         .setTitle("Daily Reward")
         .setDescription(msg)
         .setColor(BOT_GLOBAL_RGB_COLOR)
         .setTimestamp();
 
-      await Character.updateOne({ userId: user.id }, { 
-        $set: {
-          gold: gold + randomGold,
-          health: health + healthReward,
-          daily: { 
-            lastClaimed: now,
-            claimed: true 
-          }
-        } 
-      });
+      this.updateChar(user.id, char, randomGold, healthReward, now);
 
-      return message.channel.send({ embeds: [embed] });
+      return message.channel.send({ embeds: [dailyEmbed] });
     } else {
       if (cooldown - (now - lastClaimed) > 0) {  // If the cooldown is still active
         const timeLeft = ParseMS(cooldown - (now - lastClaimed));
 
-        const embed = new MessageEmbed()
+        const errorEmbed = new MessageEmbed()
           .setTitle("Daily Reward")
           .setColor(BOT_ERROR_RGB_COLOR)
           .setDescription(`You already claimed the daily reward!\nYou can claim again in \`${timeLeft}\`!`);
 
-        return message.channel.send({ embeds: [embed] });
+        return message.channel.send({ embeds: [errorEmbed] });
       } else {  // If the user has not claimed their daily
           const randomGold = Math.floor(Math.random() * 100) + 25;
           const healthReward = (health === 100) ? 0 : 25;
@@ -77,25 +67,29 @@ export class DailyCommand extends Command {
               msg += "\n» **+25**<:life_hp:853288570113359872>";
           } 
 
-          let embed = new MessageEmbed()
+          const dailyEmbed = new MessageEmbed()
             .setTitle("Daily Reward")
             .setDescription(msg)
             .setColor(BOT_GLOBAL_RGB_COLOR)
             .setTimestamp();
 
-          message.channel.send({ embeds: [embed] });
+          message.channel.send({ embeds: [dailyEmbed] });
 
-          await Character.updateOne({ userId: user.id }, { 
-            $set: {
-              gold: gold + randomGold,
-              health: health + healthReward,
-              daily: {
-                lastClaimed: now,
-                claimed: true 
-              }
-            } 
-          });
+          this.updateChar(user.id, char, randomGold, healthReward, now);
         }
     }
+  }
+
+  private async updateChar(userId: string, char: ICharacter, randomGold: number, healthReward: number, now: number) {
+    await Character.updateOne({ userId: userId }, { 
+      $set: {
+        gold: char.gold + randomGold,
+        health: char.health + healthReward,
+        daily: {
+          lastClaimed: now,
+          claimed: true 
+        }
+      } 
+    });
   }
 }

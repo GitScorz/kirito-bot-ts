f4d94df3eaf9cb1ef907a20b06dfc68610068a5b
Scorz <cristalizado6@gmail.com>
2022-04-25 13:36:00 +0100

    tweak(config): bot invite url

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -11,7 +11,7 @@ export const BOT_ERROR_RGB_COLOR: ColorResolvable = [255, 0, 0];
 export const BOT_SUCCESS_RGB_COLOR: ColorResolvable = [0, 255, 0];
 export const BOT_INVISIBLE_RGB_COLOR: ColorResolvable = [54, 57, 64];
 
-// export const BOT_INVITE_URL: string = "some url";
+export const BOT_INVITE_URL: string = "https://discord.com/api/oauth2/authorize?client_id=851514463781781545&permissions=2147483648&scope=bot";
 // export const BOT_WEBSITE_URL: string = "some website";
 
 export const BOT_PRESENCE: PresenceData = {
